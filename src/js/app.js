4121b2fb04805b9884b9b017a248b840a4d2bcb7
Alvin <yansonalvin29@gmail.com>
2021-09-12 13:34:43 +0800

    success buy token i think i need to play some dota2 now

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ App = {
     web3Provider: null,
     contracts: {},
     account: '0x0',
+    loading: false,
+    tokenPrice: 1000000000000000,
+    tokensSold: 0,
+    tokensAvailable: 750000,
 
     init: function () {
         console.log('app initialized');
@@ -35,6 +39,8 @@ App = {
                 App.contracts.DappToken.deployed().then(function (dappToken) {
                     console.log('dapp token address', dappToken.address);
                 });
+                
+                App.listenForEvents();
                 return App.render();
             });
         })
@@ -42,6 +48,17 @@ App = {
     },
 
     render: function () {
+        if (App.loading) {
+            return;
+        }
+        App.loading = true;
+        var loader = $('#loader');
+        var content = $('#content');
+
+        loader.show();
+        content.hide();
+
+
         // load account data
         web3.eth.getCoinbase(function (err, account) {
             if (err === null) {
@@ -49,6 +66,69 @@ App = {
                 App.account = account;
                 $('#accountAddress').html('Your account: ' + account);
             }
+        })
+
+        // load token sale contract
+        App.contracts.DappTokenSale.deployed().then(function (instance) {
+            dappTokenSaleInstance = instance;
+            return dappTokenSaleInstance.tokenPrice();
+        }).then(function (tokenPrice) {
+            App.tokenPrice = tokenPrice;
+            $('.token-price').html(web3.fromWei(App.tokenPrice, 'ether').toNumber());
+            return dappTokenSaleInstance.tokensSold();
+        }).then(function (tokensSold) {
+            App.tokensSold = tokensSold.toNumber();
+            $('.tokens-sold').html(App.tokensSold);
+            $('.tokens-available').html(App.tokensAvailable);
+
+            var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+            $('#progress').css('width', progressPercent + '%');
+
+            // load token contract
+            App.contracts.DappToken.deployed().then(function (instance) {
+                dappTokenInstance = instance;
+                return dappTokenInstance.balanceOf(App.account)
+            }).then(function (balance) {
+                console.log(balance.toNumber())
+                $('.dapp-balance').html(balance.toNumber());
+                App.loading = false;
+                loader.hide();
+                content.show();
+            })
+
+        })
+    },
+
+    buyTokens: function () {
+
+        $('#content').hide();
+        $('#loader').show();
+
+        var numberOfTokens = $('#numberOfTokens').val();
+
+        App.contracts.DappTokenSale.deployed().then(function (instance) {
+            return instance.buyTokens(numberOfTokens, {
+                from: App.account,
+                value: numberOfTokens * App.tokenPrice,
+                gas: 500000
+            }).then(function(result) {
+                console.log('tokens bought...')
+                $('form').trigger('reset');
+                // wait for sell event
+            })
+        })
+    },
+
+    // events emitted from contract
+    listenForEvents: function() {
+        App.contracts.DappTokenSale.deployed().then(function(instance) {
+            instance.Sell({}, {
+                fromBlock: 0,
+                toBlock: 'latest'
+            }).watch(function(err, event) {
+                console.log('event triggered', event)
+                App.render();
+            })
         })
     }
 }
